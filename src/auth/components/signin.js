abdb1ac9f8194d15cab99039b3f4f5431cb68952
siddanand <chandiboy18@gmail.com>
2021-10-30 10:11:51 +0530

    Signin Page

---
+++ src/auth/components/signin.js
@@ -0,0 +1,46 @@
+import Styles from "../styles/signin.module.css";
+import { useState } from "react";
+export default function Signin() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const onSubmithandler = (e) => {
+    e.preventDefault();
+    if (email === "admin@jobhunt.com" && password === "admin123") {
+      localStorage.setItem("email", email);
+      localStorage.setItem("password", password);
+    } else {
+      alert("enter correct details");
+    }
+  };
+  return (
+    <div className="container-fluid" id={Styles.container}>
+      <div id={Styles.box}>
+        <p id={Styles.heading}>Welcome back!</p>
+        <form onSubmit={(e) => onSubmithandler(e)}>
+          <label id={Styles.label}>Email</label>
+          <br />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id={Styles.input}
+            required
+          />
+          <br />
+          <label id={Styles.label}>Password</label>
+          <br />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            id={Styles.input}
+            required
+          />
+          <br />
+          <button id={Styles.button} type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
